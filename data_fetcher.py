827436441b134cd59e7bb1fa74a739cee8a52f9d
Peridian Tabe <peridianjoytabe1980@gmail.com>
2025-10-20 12:27:51 +0000

    created api for milestone 1 to 3

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,24 +2,22 @@ import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Load API key from .env
 
 API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://api.api-ninjas.com/v1/animals?name="
 
 def fetch_data(animal_name):
     """
-    Fetches animals data from the API.
-    Returns a list of animals (dictionaries) or an empty list if none found.
+    Fetches the animals data for the given 'animal_name'.
+    Returns a list of animals (each is a dictionary with keys: name, taxonomy, locations, characteristics)
     """
     response = requests.get(
         f"{BASE_URL}{animal_name}",
         headers={"X-Api-Key": API_KEY}
     )
-
     if response.status_code == 200:
-        data = response.json()
-        return data if data else []
+        return response.json() if response.json() else []
     else:
         print(f"Error {response.status_code}: {response.text}")
         return []
